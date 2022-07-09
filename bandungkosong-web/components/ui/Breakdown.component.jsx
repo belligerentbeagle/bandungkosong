f0de45caac49ae44b3ec055021ee4f37c237dcf6
nathanielcourses <nathaniel.courses@gmail.com>
2022-07-09 19:45:57 +0800

    Update data section of dashboard

--- bandungkosong-web/components/ui/Breakdown.component.jsx
+++ bandungkosong-web/components/ui/Breakdown.component.jsx
@@ -1,9 +1,42 @@
+import ingredientsData from "../../data/ingredients.json";
+
 const Breakdown = () => {
+  // TODO: replace with real data
+  const ingredients = ingredientsData;
+
   return (
     <>
-      <h1 className="text-3xl font-bold mb-2">June</h1>
-      <h2 className="text-xl font-medium mb-6">Prediction for July</h2>
-      <div className="h-5/6 w-full rounded-xl border"></div>
+      <h1 className="text-3xl font-bold mb-2">Breakdown</h1>
+      <h2 className="text-xl font-medium mb-6">
+        Ingredients to buy for next month
+      </h2>
+      <div className="h-5/6 w-full rounded-xl border overflow-y-scroll no-scrollbar">
+        <table className="w-full text-sm text-left text-gray-500">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+            <tr>
+              <th scope="col" className="px-6 py-3 rounded-tl-xl">
+                Ingredient
+              </th>
+              <th scope="col" className="px-6 py-3 rounded-tr-xl">
+                Quantity
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {ingredients.map((ingredient) => (
+              <tr className="bg-white border-b" key={ingredient.name}>
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                >
+                  {ingredient.name}
+                </th>
+                <td className="px-6 py-4">{ingredient.quantity}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
--- bandungkosong-web/components/ui/Data.component.jsx
+++ bandungkosong-web/components/ui/Data.component.jsx
@@ -1,9 +1,37 @@
+import { useState } from "react";
+import Modal from "./Modal.component";
+import ingredientsData from "../../data/ingredients.json";
+
 const Data = () => {
+  const [ingredients, setIngredients] = useState(ingredientsData);
+  const [isFormShown, setIsFormShown] = useState(false);
+
+  const showModalHandler = () => setIsFormShown(true);
+  const hideModalHandler = () => setIsFormShown(false);
+
+  const addIngredient = (ingredient) =>
+    setIngredients([...ingredients, ingredient]);
+
   return (
     <>
-      <h1 className="text-3xl font-bold mb-2">Your Data</h1>
-      <h2 className="text-xl font-medium mb-6">Prediction for July</h2>
+      <h1 className="text-3xl font-bold mb-4">Your Data</h1>
+      <div className="mb-6 flex items-center gap-3">
+        <button
+          className="border border-gray-400 py-1 px-3 rounded-lg flex items-center gap-2"
+          onClick={showModalHandler}
+        >
+          <span className="text-xl">&#65291;</span>
+          <span>Import</span>
+        </button>
+        <div className="text-lg">for July</div>
+      </div>
       <div className="h-5/6 w-full rounded-xl border"></div>
+      {isFormShown && (
+        <Modal
+          hideModalHandler={hideModalHandler}
+          addIngredient={addIngredient}
+        ></Modal>
+      )}
     </>
   );
 };
