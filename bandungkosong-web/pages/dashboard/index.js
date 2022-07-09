d578e7bc400ee1cd10b12cd3ec247f00f52bbdba
nathanielcourses <nathaniel.courses@gmail.com>
2022-07-09 17:58:11 +0800

    Update sidebar for dashboard

--- bandungkosong-web/pages/dashboard/index.js
+++ bandungkosong-web/pages/dashboard/index.js
@@ -1,5 +1,45 @@
+import { useEffect, useState } from "react";
+import Breakdown from "../../components/ui/Breakdown.component";
+import Data from "../../components/ui/Data.component";
+import Dishes from "../../components/ui/Dishes.component";
+import Sidebar from "../../components/ui/Sidebar.component";
+
 const Dashboard = () => {
-  return <div>Content</div>;
+  // I forgot how to do proper dynamic components
+  const [currentComponent, setCurrentComponent] = useState(
+    <Breakdown></Breakdown>
+  );
+
+  const [currentSlug, setCurrentSlug] = useState("breakdown");
+
+  useEffect(() => {
+    let newComponent;
+
+    switch (currentSlug) {
+      case "breakdown":
+        newComponent = <Breakdown></Breakdown>;
+        break;
+      case "data":
+        newComponent = <Data></Data>;
+        break;
+      case "dishes":
+        newComponent = <Dishes></Dishes>;
+        break;
+      default:
+        newComponent = <div>Error, currerntSlug incompatible</div>;
+    }
+
+    setCurrentComponent(newComponent);
+  }, [currentSlug]);
+
+  const changeRouteHandler = (slug) => setCurrentSlug(slug);
+
+  return (
+    <div className="w-full h-screen flex">
+      <Sidebar changeRouteHandler={changeRouteHandler}></Sidebar>
+      <main>{currentComponent}</main>
+    </div>
+  );
 };
 
 export default Dashboard;
